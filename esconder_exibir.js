985a6e638f6e8122f7909f431c4b0b5a8cb65b93
RFCcastro93 <116671354+RFCcastro93@users.noreply.github.com>
2022-10-25 15:06:17 -0300

    01

---
+++ esconder_exibir.js
@@ -0,0 +1,167 @@
+var visibilidade = false; //Variável que vai manipular o botão Exibir/ocultar
+
+
+            function ocultarExibir(){ // Quando clicar no botão.
+
+                if(visibilidade){//Se a variável visibilidade for igual a true, então...
+                    document.getElementById("func_botao").style.display = "none";//Ocultamos a div
+                    visibilidade = false;//alteramos o valor da variável para falso.
+                }else{//ou se a variável estiver com o valor false..
+                    document.getElementById("func_botao").style.display = "block";//Exibimos a div..
+                    visibilidade = true;//Alteramos o valor da variável para true.
+                }
+            }
+
+ function ocultarExibir2(){ // Quando clicar no botão.
+
+                if(visibilidade){//Se a variável visibilidade for igual a true, então...
+                    document.getElementById("func_botao2").style.display = "none";//Ocultamos a div
+                    visibilidade = false;//alteramos o valor da variável para falso.
+                }else{//ou se a variável estiver com o valor false..
+                    document.getElementById("func_botao2").style.display = "block";//Exibimos a div..
+                    visibilidade = true;//Alteramos o valor da variável para true.
+                }
+            }
+
+ function ocultarExibir3(){ // Quando clicar no botão.
+
+                if(visibilidade){//Se a variável visibilidade for igual a true, então...
+                    document.getElementById("func_botao3").style.display = "none";//Ocultamos a div
+                    visibilidade = false;//alteramos o valor da variável para falso.
+                }else{//ou se a variável estiver com o valor false..
+                    document.getElementById("func_botao3").style.display = "block";//Exibimos a div..
+                    visibilidade = true;//Alteramos o valor da variável para true.
+                }
+            }
+
+ function ocultarExibir4(){ // Quando clicar no botão.
+
+                if(visibilidade){//Se a variável visibilidade for igual a true, então...
+                    document.getElementById("func_botao4").style.display = "none";//Ocultamos a div
+                    visibilidade = false;//alteramos o valor da variável para falso.
+                }else{//ou se a variável estiver com o valor false..
+                    document.getElementById("func_botao4").style.display = "block";//Exibimos a div..
+                    visibilidade = true;//Alteramos o valor da variável para true.
+                }
+            } 
+
+function ocultarExibir5(){ // Quando clicar no botão. pagina de procedimentos 
+
+                if(visibilidade){//Se a variável visibilidade for igual a true, então...
+                    document.getElementById("teste").style.display = "none";//Ocultamos a div
+                    visibilidade = false;//alteramos o valor da variável para falso.
+                }else{//ou se a variável estiver com o valor false..
+                    document.getElementById("teste").style.display = "block";//Exibimos a div..
+                    visibilidade = true;//Alteramos o valor da variável para true.
+                }
+            }
+
+            function ocultarExibir6(){ // Quando clicar no botão. pagina de procedimentos 
+
+                if(visibilidade){//Se a variável visibilidade for igual a true, então...
+                    document.getElementById("teste2").style.display = "none";//Ocultamos a div
+                    visibilidade = false;//alteramos o valor da variável para falso.
+                }else{//ou se a variável estiver com o valor false..
+                    document.getElementById("teste2").style.display = "block";//Exibimos a div..
+                    visibilidade = true;//Alteramos o valor da variável para true.
+                }
+            }
+
+            function ocultarExibir7(){ // Quando clicar no botão. pagina de procedimentos 
+
+                if(visibilidade){//Se a variável visibilidade for igual a true, então...
+                    document.getElementById("teste3").style.display = "none";//Ocultamos a div
+                    visibilidade = false;//alteramos o valor da variável para falso.
+                }else{//ou se a variável estiver com o valor false..
+                    document.getElementById("teste3").style.display = "block";//Exibimos a div..
+                    visibilidade = true;//Alteramos o valor da variável para true.
+                }
+            }
+
+            function ocultarExibir8(){ // Quando clicar no botão. pagina de procedimentos 
+
+                if(visibilidade){//Se a variável visibilidade for igual a true, então...
+                    document.getElementById("teste4").style.display = "none";//Ocultamos a div
+                    visibilidade = false;//alteramos o valor da variável para falso.
+                }else{//ou se a variável estiver com o valor false..
+                    document.getElementById("teste4").style.display = "block";//Exibimos a div..
+                    visibilidade = true;//Alteramos o valor da variável para true.
+                }
+            }
+
+            function ocultarExibir9(){ // Quando clicar no botão. pagina de procedimentos 
+
+                if(visibilidade){//Se a variável visibilidade for igual a true, então...
+                    document.getElementById("teste5").style.display = "none";//Ocultamos a div
+                    visibilidade = false;//alteramos o valor da variável para falso.
+                }else{//ou se a variável estiver com o valor false..
+                    document.getElementById("teste5").style.display = "block";//Exibimos a div..
+                    visibilidade = true;//Alteramos o valor da variável para true.
+                }
+            }
+
+            function ocultarExibir10(){ // Quando clicar no botão. pagina de procedimentos 
+
+                if(visibilidade){//Se a variável visibilidade for igual a true, então...
+                    document.getElementById("teste6").style.display = "none";//Ocultamos a div
+                    visibilidade = false;//alteramos o valor da variável para falso.
+                }else{//ou se a variável estiver com o valor false..
+                    document.getElementById("teste6").style.display = "block";//Exibimos a div..
+                    visibilidade = true;//Alteramos o valor da variável para true.
+                }
+            }
+
+            function ocultarExibir11(){ // Quando clicar no botão. pagina de procedimentos 
+
+                if(visibilidade){//Se a variável visibilidade for igual a true, então...
+                    document.getElementById("teste7").style.display = "none";//Ocultamos a div
+                    visibilidade = false;//alteramos o valor da variável para falso.
+                }else{//ou se a variável estiver com o valor false..
+                    document.getElementById("teste7").style.display = "block";//Exibimos a div..
+                    visibilidade = true;//Alteramos o valor da variável para true.
+                }
+            }
+
+            function ocultarExibir12(){ // Quando clicar no botão. pagina de procedimentos 
+
+                if(visibilidade){//Se a variável visibilidade for igual a true, então...
+                    document.getElementById("teste8").style.display = "none";//Ocultamos a div
+                    visibilidade = false;//alteramos o valor da variável para falso.
+                }else{//ou se a variável estiver com o valor false..
+                    document.getElementById("teste8").style.display = "block";//Exibimos a div..
+                    visibilidade = true;//Alteramos o valor da variável para true.
+                }
+            }
+
+            function ocultarExibir13(){ // Quando clicar no botão. pagina de procedimentos 
+
+                if(visibilidade){//Se a variável visibilidade for igual a true, então...
+                    document.getElementById("teste10").style.display = "none";//Ocultamos a div
+                    visibilidade = false;//alteramos o valor da variável para falso.
+                }else{//ou se a variável estiver com o valor false..
+                    document.getElementById("teste10").style.display = "block";//Exibimos a div..
+                    visibilidade = true;//Alteramos o valor da variável para true.
+                }
+            }
+
+            function ocultarExibir14(){ // Quando clicar no botão. pagina de procedimentos 
+
+                if(visibilidade){//Se a variável visibilidade for igual a true, então...
+                    document.getElementById("teste11").style.display = "none";//Ocultamos a div
+                    visibilidade = false;//alteramos o valor da variável para falso.
+                }else{//ou se a variável estiver com o valor false..
+                    document.getElementById("teste11").style.display = "block";//Exibimos a div..
+                    visibilidade = true;//Alteramos o valor da variável para true.
+                }
+            }
+
+function ocultarExibir15(){ // Quando clicar no botão. pagina de procedimentos 
+
+                if(visibilidade){//Se a variável visibilidade for igual a true, então...
+                    document.getElementById("teste12").style.display = "none";//Ocultamos a div
+                    visibilidade = false;//alteramos o valor da variável para falso.
+                }else{//ou se a variável estiver com o valor false..
+                    document.getElementById("teste12").style.display = "block";//Exibimos a div..
+                    visibilidade = true;//Alteramos o valor da variável para true.
+                }
+            }
